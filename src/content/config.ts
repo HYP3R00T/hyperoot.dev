c2fad3fadced1a7954a092dd919494b472f4f4bb
the-hyperoot <rajesh.kumar.das.tech@gmail.com>
2023-11-24 05:16:58 +0530

    UI improvements

--- src/content/config.ts
+++ src/content/config.ts
@@ -14,6 +14,7 @@ const blog = defineCollection({
       .optional()
       .transform((str) => (str ? new Date(str) : undefined)),
     heroImage: z.string().optional(),
+    isDraft: z.boolean().optional().default(false),
   }),
 });
 
