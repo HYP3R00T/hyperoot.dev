d09a58fccbeac79d801b9005926c0b46c165086f
HYP3R00T <hyperoot.tech@proton.me>
2024-01-07 21:42:05 +0530

    upgraded astro

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,26 @@
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    category: z.string().optional(),
-    pubDate: z
-      .string()
-      .or(z.date())
-      .transform((val) => new Date(val)),
-    updatedDate: z
-      .string()
-      .optional()
-      .transform((str) => (str ? new Date(str) : undefined)),
-    heroImage: z.string().optional(),
-    isDraft: z.boolean().optional().default(false),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      category: z.string().optional(),
+      pubDate: z
+        .string()
+        .or(z.date())
+        .transform((val) => new Date(val)),
+      updatedDate: z
+        .string()
+        .optional()
+        .transform((str) => (str ? new Date(str) : undefined)),
+      heroImage: image()
+        .refine((img) => img.width >= 1080, {
+          message: "Cover image must be at least 1080 pixels wide!",
+        })
+        .optional(),
+      isDraft: z.boolean().optional().default(false),
+    }),
 });
 
 export const collections = { blog };
