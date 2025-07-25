2dfb11621dc968602b99280bf7d74ef23d54e2b2
hyperoot <rajesh@hyperoot.dev>
2025-07-25 01:07:12 +0000

    feat: add vue component for the homepage

---
+++ docs/.vitepress/theme/HomeExtra.vue
@@ -0,0 +1,46 @@
+<template>
+    <section class="extra-section">
+        <div class="extra-container">
+            <h2 class="extra-title">Explore my Projects</h2>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+.extra-section {
+    position: relative;
+    padding: 0 24px;
+}
+
+@media (min-width: 640px) {
+    .extra-section {
+        padding: 0 48px;
+    }
+}
+
+@media (min-width: 960px) {
+    .extra-section {
+        padding: 0 64px;
+    }
+}
+
+.extra-container {
+    margin: 0 auto;
+    max-width: 1152px;
+}
+
+.extra-title {
+    font-size: 1.8rem;
+    font-weight: 700;
+    line-height: 1.2;
+    margin-block-end: var(--vp-space-4);
+    padding: 2rem 0;
+    text-align: center;
+}
+
+@media (min-width: 960px) {
+    .extra-title {
+        text-align: left;
+    }
+}
+</style>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,9 @@
 import DefaultTheme from 'vitepress/theme'
 import type { Theme } from 'vitepress'
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
+import { h } from 'vue'
+
+import HomeExtra from './HomeExtra.vue'
 
 import './custom.css'
 import 'virtual:group-icons.css'
@@ -9,5 +12,10 @@ export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     enhanceAppWithTabs(app)
+  },
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'home-features-before': () => h(HomeExtra),
+    })
   },
 } satisfies Theme
