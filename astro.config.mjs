fac088fba099fb75a0499e812b1bc7d4f6746a0b
Rajesh <hyperoot.tech@proton.me>
2026-07-23 18:11:55 +0530

    feat: improve support for codeblocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,26 @@
 // @ts-check
 
+import { unified } from '@astrojs/markdown-remark'
 import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 import icon from 'astro-icon'
+import rehypeCodeBlocks from './src/lib/rehype-code-blocks.mjs'
+import rehypeHeadingLinks from './src/lib/rehype-heading-links.mjs'
+
+/** @type {import("@astrojs/markdown-remark").RehypePlugins} */
+const rehypePlugins = [[rehypeCodeBlocks, { theme: 'houston' }], rehypeHeadingLinks]
 
 export default defineConfig({
   site: 'https://hyperoot.dev',
   prefetch: true,
 
   markdown: {
-    shikiConfig: {
-      theme: 'poimandres',
-    },
+    processor: unified({
+      rehypePlugins,
+    }),
+    syntaxHighlight: false,
   },
 
   integrations: [
